584bc8c5872b3299dd05aff4c58ece59b6554017
Marcos Buganeme <marcos.after@gmail.com>
2019-07-13 16:14:52 -0300

    estrutura básica do projeto

---
+++ src/main/java/api/basica/java/spring/ApiBasicaJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package api.basica.java.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBasicaJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBasicaJavaSpringApplication.class, args);
+	}
+
+}
